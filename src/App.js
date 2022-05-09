efad179be1f45ba8fc64623d4f367abf7539f54c
Bogdan Alexa <Bogdan Alexa>
2022-05-09 20:03:18 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { render } from "@testing-library/react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: "Frankestein",
+          id: "asc1",
+        },
+        {
+          name: "Dracula",
+          id: "asc2",
+        },
+        {
+          name: "Zombie",
+          id: "asc3",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map(monster => <h1 key = {monster.id}> { monster.name } </h1>)
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
